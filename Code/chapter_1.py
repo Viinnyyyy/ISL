ee1ac3371dd53c81f487b5b00dc4098b98b700ac
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2024-09-28 08:40:19 +0100

    Continuing the assignment on college dataset

--- Code/chapter_1.py
+++ Code/chapter_1.py
@@ -1,15 +1,47 @@
 # import ISLP
-# import matplotlib.pyplot as plt
-# from matplotlib.pyplot import subplots
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import subplots
 # import numpy as np
 import pandas as pd
 
-df2 = pd.read_csv(
-    "/Users/mac/Documents/ISL_with_Python/ALL_CSV_FILES/College.csv")
-df3 = df2.rename({'Unnamed: 0': 'College'}, axis=1)
-df3 = df3.set_index('College')
-df = df3
-df.head()
+college2 = pd.read_csv(
+    "/Users/mac/Documents/ISL_with_Python/ISL/ALL_CSV_FILES/College.csv")
+college3 = college2.rename({'Unnamed: 0': 'College'}, axis=1)
+college3 = college2.set_index('College')
+college = college2
+college.head()
 
 
-df.dtypes  # output shows that Private is a string instead of Boolean
+college.dtypes  # output shows that Private is a string instead of Boolean
+college.describe()
+
+# Plotting using pandas plotting scatter matrix
+fig, ax = subplots(figsize=(8, 8))
+cols = ['Top10perc', 'Apps', 'Enroll']
+pd.plotting.scatter_matrix(college[cols])
+plt.show()
+
+fig, ax = subplots(figsize=(8, 8))
+college.boxplot('Outstate', by='Private', ax=ax)
+plt.show()
+
+college['Top10perc'].unique()
+college['Elite'] = pd.cut(college['Top10perc'], [
+                          0, 50, 100], labels=['No', 'Yes'])
+# We are using [0, 50, 100] instead of [0, 0.5, 1]
+
+college['Elite'].value_counts()
+college['Elite']
+
+fig, ax = subplots(figsize=(8, 8))
+college.boxplot('Outstate', by='Elite', ax=ax)
+plt.show()
+
+college[['Top10perc', 'Elite']].head()
+
+
+# To show
+fig, ax = subplots(figsize=(8, 8))
+plt.subplots(2, 2)
+college.hist('Apps', color='red', bins=10, ax=ax)
+plt.show()
